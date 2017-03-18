c2999f1dc75d6c159431f0b15e2801f44220877a
Theo Massard <massar_t@etna-alternance.net>
2017-03-18 15:28:39 +0100

    [UPD] update bob

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -1,15 +1,10 @@
-import re
-
-
 def hey(sentence):
     sentence = sentence.rstrip('\t\n ')
-    if len(sentence) == 0:
-        return "Fine. Be that way!"
-    elif sentence == sentence.upper() and not sentence[-1] == '?':
+    if sentence.strip() == '':
+        return 'Fine. Be that way!'
+    elif sentence.isupper():
         return 'Whoa, chill out!'
-    elif sentence[-1] == '?':
-        return "Sure."
-    elif not re.search('[a-zA-Z]', sentence):
-        return 'Whatever.'
-    else:
-        return "Whatever."
+    elif sentence.endswith('?'):
+        return 'Sure.'
+
+    return 'Whatever.'
