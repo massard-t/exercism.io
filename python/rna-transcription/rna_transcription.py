e479cb6133e2434a63e088b6d02a2de09a4292ea
Theo Massard <massar_t@etna-alternance.net>
2017-03-19 12:25:56 +0100

    [UPD] SBG

--- python/rna-transcription/rna_transcription.py
+++ python/rna-transcription/rna_transcription.py
@@ -1,10 +1,5 @@
 def to_rna(dna):
-    d = {
-            'G': 'C',
-            'C': 'G',
-            'T': 'A',
-            'A': 'U'
-            }
+    d = {'G': 'C', 'C': 'G', 'T': 'A', 'A': 'U'}
     try:
         return ''.join([d[e] for e in dna])
     except KeyError:
