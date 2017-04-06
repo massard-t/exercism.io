33a1cc8d15df1fe64fb77d6a01366918aa12c200
Theo Massard <massar_t@etna-alternance.net>
2017-04-06 17:44:42 +0200

    [UPD] yay

--- rust/gigasecond/src/lib.rs
+++ rust/gigasecond/src/lib.rs
@@ -3,6 +3,5 @@ use chrono::*;
 
 
 pub fn after(date: DateTime<UTC>) -> DateTime<UTC> {
-    let gigasecond = Duration::seconds(1000000000);
-    return date + gigasecond;
+    date + Duration::seconds(1000000000)
 }
