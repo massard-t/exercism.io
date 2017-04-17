d7e7eb80d234928ce8f0958e8b3df78c9475ef9a
Theo Massard <massar_t@etna-alternance.net>
2017-04-17 11:49:05 +0200

    [UPD]

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # exercism.io
-Exercises used to learn languages from the website exercism.io
+Exercises used to learn languages from the website [exercism.io](https://exercism.io)
+
+### Current languages
+
+- [Python](python)
+- [Golang](go)
+- [Bash](bash)
+- [Haskell](haskell)
+- [PHP](php)
+- [Rust](rust)
---
+++ go/clock/clock.go
@@ -0,0 +1,35 @@
+// Clock stub file
+
+// To use the right term, this is the package *clause*.
+// You can document general stuff about the package here if you like.
+package clock
+
+import "fmt"
+
+// The value of testVersion here must match `targetTestVersion` in the file
+// clock_test.go.
+const testVersion = 4
+
+// Clock API as stub definitions.  No, it doesn't compile yet.
+// More details and hints are in clock_test.go.
+
+type Clock int
+
+func New(hour, minute int) Clock {
+	time := (hour*60 + minute) % (60 * 24)
+	if time < 0 {
+		time += 60 * 24
+	}
+	return Clock(time)
+}
+
+func (c Clock) String() string {
+	return fmt.Sprintf("%02d:%02d", c/60, c%60)
+}
+
+func (c Clock) Add(minutes int) Clock {
+	return New(0, int(c)+minutes)
+}
+
+// Remember to delete all of the stub comments.
+// They are just noise, and reviewers will complain.
